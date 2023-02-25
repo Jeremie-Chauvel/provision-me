bab2aaacd51a49aea90bb9f4468b0a89e504164e
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2023-02-25 17:11:03 +0100

    Remove i3 from installation

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ source ./install/redshift.sh
 source ./install/httpie.sh
 source ./install/xclip.sh
 source ./install/aspell.sh
-source ./install/i3.sh
+#source ./install/i3.sh
 source ./install/clementine.sh
 source ./install/polybar.sh
 source ./install/docker.sh
