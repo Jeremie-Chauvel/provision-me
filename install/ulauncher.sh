c43400bc271385dbf753f153d9aa0194aaa3edd1
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2023-03-11 17:05:24 +0100

    Ulauncher script improvement

--- install/ulauncher.sh
+++ install/ulauncher.sh
@@ -3,11 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# nix-env -iA nixpkgs.wmctrl
-
 sudo apt install --yes curl
 readonly ulauncher_downloads_urls=$(curl -s https://api.github.com/repos/Ulauncher/Ulauncher/releases/latest | jq '.assets | map(.browser_download_url)')
 readonly ulauncher_deb_download_url=$(echo "$ulauncher_downloads_urls" | awk '/deb/ {print $1}' | sed -e 's/"//g' -e 's/,//g')
-curl -o ./ulauncher.deb -L "$ulauncher_deb_download_url"
+
+curl -fsSL "$ulauncher_deb_download_url" -o ./ulauncher.deb
 sudo apt install ./ulauncher.deb --yes
 rm ./ulauncher.deb
