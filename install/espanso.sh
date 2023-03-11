9110824fad76921f9b0c3a4ed768d677f15bd7f8
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2023-03-11 17:07:47 +0100

    Add espanso

---
+++ install/espanso.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# install espanso
+set -euo pipefail
+IFS=$'\n\t'
+
+# TODO wayland test
+sudo apt install --yes curl
+curl -fsSL https://github.com/federico-terzi/espanso/releases/download/v2.1.8/espanso-debian-wayland-amd64.deb -o ./espanso.deb
+sudo apt install ./espanso.deb --yes
+rm ./espanso.deb
+sudo setcap "cap_dac_override+p" $(which espanso)
+espanso register
+espanso start
