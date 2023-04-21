9ab89890ad2838601f99494e2fb8d92077a155a4
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2023-04-21 13:32:18 +0200

    chore: update flameshot install

--- install/flameshot.sh
+++ install/flameshot.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-sudo apt install --yes flameshot
-
+# troubleshoot https://github.com/emersion/xdg-desktop-portal-wlr/wiki/%22It-doesn't-work%22-Troubleshooting-Checklist/f8d89b8a2f9d61ba318c8be075b46de230c7d97e
+sudo apt install flameshot xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr
 # shotcut to setup in your dotfiles:
 # flameshot gui
-# eg for i3: `bindsym Print exec flameshot gui``
+# eg for sway/i3: `bindsym Print exec flameshot gui``
