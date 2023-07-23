d25e7ef558fd05eb23fb518e0158f54fd12243e1
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2023-07-23 18:25:09 +0200

    update flameshot install for pop os

--- install/flameshot.sh
+++ install/flameshot.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # troubleshoot https://github.com/emersion/xdg-desktop-portal-wlr/wiki/%22It-doesn't-work%22-Troubleshooting-Checklist/f8d89b8a2f9d61ba318c8be075b46de230c7d97e
-sudo apt install flameshot xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr
+# sudo apt install flameshot -y xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr # for sway
+sudo apt install flameshot -y # pop os
 # shotcut to setup in your dotfiles:
 # flameshot gui
 # eg for sway/i3: `bindsym Print exec flameshot gui``
