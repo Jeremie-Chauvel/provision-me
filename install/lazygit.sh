efba05e23912bafc1b26c483041bfdfc30ab0230
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2024-09-18 10:55:58 +0200

    add lazygit

---
+++ install/lazygit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# this script install lazygit
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$SCRIPT_DIRECTORY/utils/early_exit_if_command_exist.sh"
+early_exit_if_command_exist lazygit
+
+sudo apt install --yes curl
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+rm -f lazygit lazygit.tar.gz
