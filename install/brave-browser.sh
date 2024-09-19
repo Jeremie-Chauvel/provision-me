8a04fa5e9058a4bac2fd762794f3f2addbabd229
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2024-09-19 09:00:16 +0200

    add brave

---
+++ install/brave-browser.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# this script install brave-browser
+
+set -euo pipefail
+IFS=$'\n\t'
+SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$SCRIPT_DIRECTORY/utils/early_exit_if_command_exist.sh"
+early_exit_if_command_exist brave-browser
+
+sudo apt install curl -y
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser -y
