594bfd024713948e2a7d19b6b660cc5c613ca9b2
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2024-09-20 15:21:44 +0200

    add ubuntu keymap

---
+++ config/keymap-ubuntu.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# setup some keybindings for ubuntu
+
+if (("Ubuntu" != $(lsb_release -si))); then
+  echo "not on Ubuntu, skipping"
+  exit 0
+fi
+
+# remove dash-to-dock binding super for nothing
+for e in {1..10}; do
+  gsettings set org.gnome.shell.extensions.dash-to-dock "app-ctrl-hotkey-$e" \[\]
+  gsettings set org.gnome.shell.extensions.dash-to-dock "app-shift-hotkey-$e" \[\]
+  gsettings set org.gnome.shell.extensions.dash-to-dock "app-hotkey-$e" \[\]
+done
+
+gsettings set org.gnome.desktop.wm.keybindings "close" "['<Shift><Super>a']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-1" "['<Shift><Super>y']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-2" "['<Shift><Super>u']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-3" "['<Shift><Super>i']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-4" "['<Shift><Super>o']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-last" "['<Shift><Super>p']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-1" "['<Super>y']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-2" "['<Super>u']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-3" "['<Super>i']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-4" "['<Super>o']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-down" "['<Primary><Super>Down', '<Primary><Super>j']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-last" "['<Super>p']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-left" "['<Super>Left']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-right" "['<Super>Right']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-up" "['<Primary><Super>Up', '<Primary><Super>k']"
+gsettings set org.gnome.desktop.wm.keybindings "toggle-fullscreen" "['<Super>f']"
