561e298f888b30e9890c8714c471d2430cc144f2
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2024-10-08 10:33:31 +0200

    keymap azerty ubuntu

--- config/keymap-ubuntu.sh
+++ config/keymap-ubuntu.sh
@@ -14,6 +14,13 @@ for e in {1..10}; do
 done
 
 gsettings set org.gnome.desktop.wm.keybindings "close" "['<Shift><Super>a']"
+gsettings set org.gnome.desktop.wm.keybindings "toggle-fullscreen" "['<Super>f']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-down" "['<Primary><Super>Down', '<Primary><Super>j']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-left" "['<Super>Left']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-right" "['<Super>Right']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-up" "['<Primary><Super>Up', '<Primary><Super>k']"
+
+#qwerty
 gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-1" "['<Shift><Super>y']"
 gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-2" "['<Shift><Super>u']"
 gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-3" "['<Shift><Super>i']"
@@ -23,9 +30,16 @@ gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-1" "['<Super
 gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-2" "['<Super>u']"
 gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-3" "['<Super>i']"
 gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-4" "['<Super>o']"
-gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-down" "['<Primary><Super>Down', '<Primary><Super>j']"
 gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-last" "['<Super>p']"
-gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-left" "['<Super>Left']"
-gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-right" "['<Super>Right']"
-gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-up" "['<Primary><Super>Up', '<Primary><Super>k']"
-gsettings set org.gnome.desktop.wm.keybindings "toggle-fullscreen" "['<Super>f']"
+
+# azerty
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-1" "['<Shift><Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-2" "['<Shift><Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-3" "['<Shift><Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-4" "['<Shift><Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings "move-to-workspace-last" "['<Shift><Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-1" "['<Super>ampersand']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-2" "['<Super>eacute']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-3" "['<Super>quotedbl']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-4" "['<Super>apostrophe']"
+gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-last" "['<Super>parenleft']"
