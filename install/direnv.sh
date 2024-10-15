947e92c9b7de905f7d83814396e01c3671f8dc91
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2024-10-15 11:45:17 +0200

    update direnv install

--- install/direnv.sh
+++ install/direnv.sh
@@ -9,4 +9,6 @@ installer_name="direnv-installer.sh"
 curl -sfL https://direnv.net/install.sh >"$installer_name"
 chmod +x "$installer_name"
 bin_path="$HOME/.local/bin" bash "$installer_name"
+sudo rm -f '/usr/bin/direnv'
+sudo mv "$HOME/.local/bin/direnv" '/usr/bin/direnv'
 rm "$installer_name"
