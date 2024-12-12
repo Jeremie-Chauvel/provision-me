780d96fa687867bb6ee3b4ad93550ff3708dc1b9
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2024-12-12 10:35:48 +0100

    fix: httpie install (v3)

--- install/httpie.sh
+++ install/httpie.sh
@@ -3,4 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$SCRIPT_DIRECTORY/utils/early_exit_if_command_exist.sh"
+early_exit_if_command_exist http
+
+curl -SsL https://packages.httpie.io/deb/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/httpie.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/httpie.gpg] https://packages.httpie.io/deb ./" | sudo tee /etc/apt/sources.list.d/httpie.list >/dev/null
+sudo apt update
 sudo apt install --yes httpie
