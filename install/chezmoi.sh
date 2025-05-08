1a232f58b83d634673ae3568e112673d54e14dd3
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2025-05-09 01:04:40 +0200

    wip

--- install/chezmoi.sh
+++ install/chezmoi.sh
@@ -7,16 +7,7 @@ IFS=$'\n\t'
 chezmoi_repository=$1
 set -u
 
-SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-source "$SCRIPT_DIRECTORY/utils/early_exit_if_command_exist.sh"
-early_exit_if_command_exist chezmoi
-
-readonly chezmoi_install_dir="$HOME/.local/bin"
-
-
-if [[ ! -f ~/.local/bin ]]; then
-  mkdir -p ~/.local/bin
-fi
+brew install chezmoi
 
 chezmoi_template="$HOME/.config/chezmoi/chezmoi.toml"
 if [ ! -f "$chezmoi_template" ];then
@@ -25,13 +16,5 @@ if [ ! -f "$chezmoi_template" ];then
       gpg_signingkey='~/.ssh/id_ed25519'
       gpg_format='ssh'
   " > "$chezmoi_template"
-fi
-
-
-# install and init chezmoi using your dotfile
-if [ -n "$chezmoi_repository" ]; then
-  sh -c "$(curl -fsLS git.io/chezmoi)" -- -b "$chezmoi_install_dir" init --apply $chezmoi_repository
-else
-  # install chezmoi
-  sh -c "$(curl -fsLS get.chezmoi.io)"
+  chezmoi init --apply $chezmoi_repository
 fi
